14a8a26f92fb19176ec485e4eb8b87d17eb5e7eb
dannyboy1044 <32443655+dannyboy1044@users.noreply.github.com>
2022-03-31 23:07:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,18 @@ for(let i=0; i< size; i++){
   document.getElementsByClassName("drum")[i].addEventListener("click", function() {
     let buttonInnerHTML = this.textContent;
 
-    switch (buttonInnerHTML) {
-      case "a":
-        let sound = new Audio("sounds/kick-bass.mp3");
-        sound.play();
-        break;
-      case "s":
-        let sound2 = new Audio("sounds/snare.mp3");
-        sound2.play();
-        break;
-      case "d":
-        let sound3 = new Audio("sounds/crash.mp3");
-        sound3.play();
-        break;
-      case "f":
-        let sound4 = new Audio("sounds/tom-1.mp3");
-        sound4.play();
-        break;
-      default:
-        break;
-    }
+    makeSound(buttonInnerHTML);
+    btnAnimation(buttonInnerHTML);
   });
 }
 
 document.addEventListener("keypress", function(event) {
-  switch (event.key) {
+  makeSound(event.key);
+  btnAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
     case "a":
       let sound = new Audio("sounds/kick-bass.mp3");
       sound.play();
@@ -48,4 +35,12 @@ document.addEventListener("keypress", function(event) {
     default:
       break;
   }
-});
+}
+
+function btnAnimation(key){
+  let activeBtn = document.querySelector("."+key);
+  activeBtn.classList.add("pressed");
+  setTimeout(function () {
+    activeBtn.classList.remove("pressed");
+  }, 500);
+}
